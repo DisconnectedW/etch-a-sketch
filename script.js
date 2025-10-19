39564041419df80b922535e1e8d77428e33e2c8e
DisconnectedW <238344850+DisconnectedW@users.noreply.github.com>
2025-10-19 16:27:09 +0100

    Files have been updated The sketching functionality has been implement with only a black color for now

--- script.js
+++ script.js
@@ -1,24 +1,25 @@
 const grid = document.querySelector(".container");
-const gridSizeBtn = document.getElementById("grid-btn")
+const gridSizeBtn = document.getElementById("grid-btn");
+const clickMode = document.getElementById("click-btn");
+const dragMode = document.getElementById("drag-btn");
+const isDisabled = true;
 
 function gridCreation(num) {
     let i = 0;
     while (i < (num * num)) {
         let box = document.createElement("div");
-        box.style.width = `calc(960px / ${num} )`;
-        box.style.height = `calc(960px / ${num})`;
+        box.style.minWidth = `calc(960px / ${num} )`;
+        box.style.minHeight = `calc(960px / ${num})`;
+        box.classList.add("grid-box");
         grid.appendChild(box);
         i++;
     }
 }
 
-
-
 document.addEventListener("DOMContentLoaded", () => {
-    gridCreation(16)
-})
+    gridCreation(16);
 
-gridSizeBtn.addEventListener("click", () => {
+    gridSizeBtn.addEventListener("click", () => {
     const gridSize = parseInt(prompt("Enter a number from 1-100 for the grid size"));
     grid.innerHTML = ""
 
@@ -30,3 +31,19 @@ gridSizeBtn.addEventListener("click", () => {
         gridCreation(gridSize);
     }
 })
+
+const boxes = grid.querySelectorAll(".grid-box")
+console.log(boxes)
+
+boxes.forEach(box => {
+    box.addEventListener("mouseenter", (e) => {
+        e.target.style.backgroundColor = "#000"
+    })
+})
+
+})
+
+
+
+
+
