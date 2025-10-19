b27673de1c3c3f27b8382d9ddcec56f1eeaa0e04
DisconnectedW <238344850+DisconnectedW@users.noreply.github.com>
2025-10-19 14:50:01 +0100

    Files have been updated The functionality of the button to change the grid-size has been added

--- script.js
+++ script.js
@@ -12,6 +12,21 @@ function gridCreation(num) {
     }
 }
 
+
+
 document.addEventListener("DOMContentLoaded", () => {
     gridCreation(16)
+})
+
+gridSizeBtn.addEventListener("click", () => {
+    const gridSize = parseInt(prompt("Enter a number from 1-100 for the grid size"));
+    grid.innerHTML = ""
+
+    if(Number.isNaN(gridSize) === true) {
+        alert("Please enter a valid number");
+    } else if (gridSize < 0 || gridSize > 100) {
+        alert("Please enter a number within the range of 1-100");
+    } else {
+        gridCreation(gridSize);
+    }
 })
